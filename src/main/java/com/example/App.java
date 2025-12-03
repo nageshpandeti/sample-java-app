d901ef511a4546e8884468b8e2f9458c03049e4e
Nagesh Pandeti <99116013+nageshpandeti@users.noreply.github.com>
2025-12-03 20:05:05 +0530

    Fix greeting message in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
-// App.java
+package com.example;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World11!");
+        System.out.println("Hello, World!");
+        // Your application logic goes here
     }
 }
