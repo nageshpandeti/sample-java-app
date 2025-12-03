26bfa18d3671a71ac5b675226a287cb53f7ba2e9
Nagesh Pandeti <99116013+nageshpandeti@users.noreply.github.com>
2025-12-03 20:17:34 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,15 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        // Your application logic goes here
+        System.out.println("Hello, Kubernetes! Application is running.");
+        
+        // Keep the app alive so the container doesn’t exit immediately
+        while (true) {
+            try {
+                Thread.sleep(60000); // sleep 1 minute
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
